f3d1c798f11eebf12de834362789df474d380b13
mohit99m <44992652+mohit99m@users.noreply.github.com>
2019-08-26 21:53:01 +0530

    Add files via upload

---
+++ mohit.h
@@ -0,0 +1,75 @@
+//#include<iostream>
+#include<stdlib.h>
+#include<string.h>
+
+using namespace std;
+
+/*struct hufftree
+{
+    char c;
+    int fr;
+    struct hufftree *left, *right;
+}*root;*/
+
+struct hufftree *root;
+
+char encoded[128][16];
+
+//int TheGivenList[127];
+
+void tree_creator(int TheGivenList[128])
+{
+    int counter = 0;
+    for(int i = 0; i < 128; i++)
+    {
+        if(TheGivenList[i]!=0)
+        {
+            struct hufftree *temp = (struct hufftree *)malloc(sizeof(struct hufftree));
+            temp->c = i;
+            temp->fr = TheGivenList[i];
+            temp->left=NULL;
+            temp->right=NULL;
+            push(temp, TheGivenList[i]);
+            counter++;
+        }
+    }
+    view();
+    for(int i = 0; i < counter-1; i++)
+    {
+        struct hufftree *temp = (struct hufftree *)malloc(sizeof(struct hufftree));
+        temp->left = pop();
+        temp->right = pop();
+        cout<<temp->left->c<<" "<<temp->left->fr<<endl;
+        cout<<temp->right->c<<" "<<temp->right->fr<<endl;
+        temp->c=' ';
+        temp->fr = temp->left->fr + temp->right->fr;
+        push(temp, temp->fr);
+        view();
+    }
+    root = pop();
+}
+
+void hufftree_parser(struct hufftree *root, char encode[16])
+{
+    if(root->left==NULL && root->right==NULL)
+    {
+        strcpy(encoded[root->c],encode);
+        return;
+    }
+    char tmp1[]={'\0'};
+    strcpy(tmp1,encode);strcat(tmp1,"0");
+    hufftree_parser(root->left,tmp1);
+    char tmp2[]={'\0'};
+    strcpy(tmp2,encode);strcat(tmp2,"1");
+    hufftree_parser(root->right,tmp2);
+}
+
+void treeprint(hufftree *tnd)
+{
+    if(tnd==NULL)
+        return;
+
+    treeprint(tnd->left);
+    cout<<tnd->c<<" "<<tnd->fr<<endl;
+    treeprint(tnd->right);
+}
